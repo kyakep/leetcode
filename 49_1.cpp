579a80a3fda92e54b089ae33264e4070e0a70ebc
Jung Dong Wook <aleksanderchun24@gmail.com>
2023-05-31 20:40:48 +0300

    update

--- 49_1.cpp
+++ 49_1.cpp
@@ -1,1 +1,32 @@
+/*Given an array of strings strs, group the anagrams together. 
+You can return the answer in any order.
 
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+*/
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<std::vector<std::string> > groupAnagrams(std::vector<std::string>& strs) {
+        std::vector<std::vector<std::string> > ans; //двумерная матрица i - набор букв, j - конкретное слово
+        std::unordered_map<std::string, int> mapp; //мапа стринг-инт стринг - набор букв, инт - итератор
+        for(int i = 0, j = 0; i < strs.size(); i++) {
+            std::string tempstr = strs[i]; //присваиваем стрингу значение i-того стринга вектора strs[i], чтобы потом отсортировать без последствий
+            sort(tempstr.begin(), tempstr.end()); //сортировка чаров этого стринга
+            if(mapp.find(tempstr) != mapp.end()) { // он по мапе ищет набор букв
+                ans[mapp[tempstr]].push_back(strs[i]); //в скобках ans находится итератор при значении стринга tempstr, туда 
+            }
+            else { //если удовлетворяет условию
+                mapp[tempstr] = j; //итератор добавляемого элемента под набор букв
+                ans[mapp[tempstr]].push_back(strs[i]); 
+                j++;
+            }     
+        }
+        return ans;
+    }
+};
--- check.cpp
+++ check.cpp
@@ -1,17 +1,25 @@
-#include <vector>
 #include <string>
+#include <vector>
 #include <iostream>
+#include <unordered_map>
 
 class Solution {
 public:
-    bool isIsomorphic(std::string s, std::string t) {
-        std::unordered_map<char, char> mp, mp2;
-        for (int i=0; i<s.length(); ++i) {
-            if (mp[s[i]] && mp[s[i]]!=t[i]) return false;
-            if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
-            mp[s[i]]=t[i];
-            mp2[t[i]]=s[i];
+    std::vector<std::vector<std::string> > groupAnagrams(std::vector<std::string>& strs) {
+        std::vector<std::vector<std::string> > ans; //двумерная матрица i - набор букв, j - конкретное слово
+        std::unordered_map<std::string, int> mapp; //мапа стринг-инт стринг - набор букв, инт - итератор
+        for(int i = 0, j = 0; i < strs.size(); i++) {
+            std::string tempstr = strs[i]; //присваиваем стрингу значение i-того стринга вектора strs[i], чтобы потом отсортировать без последствий
+            sort(tempstr.begin(), tempstr.end()); //сортировка чаров этого стринга
+            if(mapp.find(tempstr) != mapp.end()) { // он по мапе ищет набор букв
+                ans[mapp[tempstr]].push_back(strs[i]); //в скобках ans находится итератор при значении стринга tempstr, туда 
+            }
+            else { //если удовлетворяет условию
+                mapp[tempstr] = j; //итератор добавляемого элемента под набор букв
+                ans[mapp[tempstr]].push_back(strs[i]); 
+                j++;
+            }     
         }
-        return true;
+        return ans;
     }
 };
